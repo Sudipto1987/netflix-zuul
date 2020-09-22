e67fa9128a09cec91fa578b4f1aa15ce7d95e2ac
Sudipto <sudipto.roy.babun.004@gmail.com>
2020-09-22 13:52:51 +0800

    test if commit is working

--- Desktop/limits-service/src/main/java/com/sudipto/microservice/limitsservice/LimitsConfigurationController.java
+++ Desktop/limits-service/src/main/java/com/sudipto/microservice/limitsservice/LimitsConfigurationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.sudipto.microservice.limitsservice.bean.LimitConfiguration;
 
-
+//making a test change in limits
 @RestController
 public class LimitsConfigurationController {
 	
